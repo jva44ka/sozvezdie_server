6f1df7a3cdb879f2426779e1030ef6bb81f0bc89
DESKTOP-5LFPUAC\stalk <zaparivannyi@gmail.com>
2020-11-07 21:48:19 +0300

    Created main architecture

---
+++ Infrastructure/Managers/AppSettingsManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure
+{
+    public class AppSettingsManager
+    {
+        public string ClientUrl { get; set; }
+        public AppSettingsManager(IConfiguration configuration)
+        {
+            ClientUrl = configuration.GetValue<string>(nameof(ClientUrl));
+        }
+    }
+}
