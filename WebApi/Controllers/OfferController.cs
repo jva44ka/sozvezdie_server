f229bd13ffee59578c4e1ebb70880709b0115150
DESKTOP-5LFPUAC\stalk <zaparivannyi@gmail.com>
2020-11-15 20:06:12 +0300

    Removed useless properties from offerListItem viewmodel

--- WebApi/Controllers/OfferController.cs
+++ WebApi/Controllers/OfferController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
-using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using Services.Interfaces;
 using WebApi.ViewModels;
